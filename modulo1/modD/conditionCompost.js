53341801899b399ba54e7dba085d4f8d6b21ebae
pedroH-fullstack <224280645+pedroHenrique-web@users.noreply.github.com>
2025-09-29 12:40:42 -0300

    conditons

---
+++ modulo1/modD/conditionCompost.js
@@ -0,0 +1,13 @@
+//i want to buy one coca
+
+var money = 5
+var coca = 12.99
+
+if (money >= coca){
+    console.log(`the price of coca was ${`$` + coca}`);
+    console.log('i bought the coca yeeeeeeeeeee');
+    
+}else{
+    console.log(`the price of coca was ${`$` + coca}`);
+    console.log(`danm, is so extended, i´m not going to buy this`);
+}
---
+++ modulo1/modD/conditionSimple.js
@@ -0,0 +1,11 @@
+var number = 4
+
+//simple condition
+if (number >= 4){
+    number += 4
+}
+
+console.log(number);
+
+
+
